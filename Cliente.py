013e1ef9efb769a63d4c8d951aa2d3dd7a514c50
Fiona Abad <fionaabad13@gmail.com>
2025-09-19 17:12:27 +0200

    cliente def

--- Cliente.py
+++ Cliente.py
@@ -8,3 +8,7 @@ class Cliente:
         self._apellido = apellido
     def GetNombre(self):
         return self._nombre
+    def GetApellido(self):
+        return self._apellido
+    def __str__(self):
+        return f'Mi nombre es {self.GetNombre()} {self.GetApellido()}'
