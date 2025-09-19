b0fc0d3e4deca91b0a8387a68b101cd19b5226ba
Fiona Abad <fionaabad13@gmail.com>
2025-09-19 16:57:53 +0200

    cliente

---
+++ Cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+    def __init__(self, nombre, apellido):
+        self._nombre = nombre
+        self._apellido = apellido
+    def SetNombre(self, nombre):
+        self._nombre = nombre    
+    def SetApellido(self, apellido):
+        self._apellido = apellido
