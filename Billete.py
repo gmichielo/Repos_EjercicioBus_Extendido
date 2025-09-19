ff1037df431a76cc3455e3c5901b07f75a3ed460
Gabriel Michielon <gabrielmichelon1612@gmail.com>
2025-09-19 17:22:28 +0200

    Eliminacion Administracion y Seteos Billite

--- Billete.py
+++ Billete.py
@@ -1,5 +1,25 @@
+from Cliente import Cliente
+
 class Billete:
     def __init__(self, bus, cliente, num_billete):
         self._busId = bus
         self._cliente = cliente
-        self._num_billte = num_billete
+        self._num_billete = num_billete
+    
+    def GetBus(self):
+        return self._busId
+    
+    def GetCliente(self):
+        return self._cliente
+    
+    def GetNum_Billete(self):
+        return self._num_billete
+    
+    def SetBus(self, bus):
+        self._busId = bus
+
+    def SetCliente(self, cliente):
+        self._cliente = cliente
+    
+    def SetNum_Billete(self, num):
+        self._num_billete = num
